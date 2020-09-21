45f64e2a85e957b432863f28d3ce6d45d7786234
Nguyễn Trần Phương Vy <nguyentranphuongvy99@gmail.com>
2020-09-21 18:16:48 +0700

    update21/09

---
+++ WebSelling/Controllers/InformationController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebSelling.Models;
+
+namespace WebSelling.Controllers
+{
+    public class InformationController : Controller
+    {
+        WebSellingEntities db = new WebSellingEntities();
+        String ViewHome = "/";
+        String strHome = "/Home/Index";
+        String ViewAcc = "/";
+        String strAcc = "/Account/Login";
+        // GET: Information
+        public ActionResult InforUser()
+        {
+            User user = (User)Session["user"];
+            if (user == null)
+            {
+                return Redirect(ViewAcc);
+            }
+            return View(/*db.Products.Where(n => n .Product_Activate == true && n.User_ID == user.User_ID).OrderByDescending(n => n.Product_DateCreate).ToList()*/);
+        }
+    }
+}
